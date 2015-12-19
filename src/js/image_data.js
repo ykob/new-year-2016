587dbbb845de9d170e1ca75fca7e3d2d830b936e
yoichi kobayashi <info@tplh.net>
2015-12-19 23:22:16 +0900

    add gulp file.

---
+++ src/js/image_data.js
@@ -0,0 +1,183 @@
+var Util = require('./modules/util');
+var Mover = require('./modules/mover');
+var glslify = require('glslify');
+var Points = require('./modules/points.js');
+var vs = glslify('./points.vs');
+var fs = glslify('./points.fs');
+
+var exports = function(){
+  var Sketch = function() {};
+  var image = new Image();
+  var image_vertices = [];
+  var movers = [];
+  var positions = null;
+  var colors = null;
+  var opacities = null;
+  var sizes = null;
+  var length_side = 400;
+  var points = new Points();
+  var created_points = false;
+
+  var loadImage = function(callback) {
+    image.src = './img/elephant.png';
+    image.onload = function() {
+      callback();
+    };
+  };
+
+  var getImageData = function() {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = length_side;
+    canvas.height = length_side;
+    ctx.drawImage(image, 0, 0);
+    var image_data = ctx.getImageData(0, 0, length_side, length_side);
+    for (var y = 0; y < length_side; y++) {
+      if (y % 3 > 0) continue;
+      for (var x = 0; x < length_side; x++) {
+        if (x % 3 > 0) continue;
+        if(image_data.data[(x + y * length_side) * 4] > 0) {
+          image_vertices.push(0, (y - length_side / 2) * -1, (x - length_side/ 2) * -1);
+        }
+      }
+    }
+  };
+
+  var buildPoints = function(scene) {
+    positions = new Float32Array(image_vertices);
+    colors = new Float32Array(image_vertices.length);
+    opacities = new Float32Array(image_vertices.length / 3);
+    sizes = new Float32Array(image_vertices.length / 3);
+    for (var i = 0; i < image_vertices.length / 3; i++) {
+      var mover = new Mover();
+      var color = new THREE.Color(
+                                  'hsl(' + (image_vertices[i * 3 + 2] + image_vertices[i * 3 + 1] + length_side) / 5
+                                  + ', 60%, 80%)');
+      mover.init(new THREE.Vector3(image_vertices[i * 3], image_vertices[i * 3 + 1], image_vertices[i * 3 + 2]));
+      mover.is_activate = true;
+      movers.push(mover);
+      color.toArray(colors, i * 3);
+      opacities[i] = 1;
+      sizes[i] = 12;
+    }
+    points.init({
+      scene: scene,
+      vs: vs,
+      fs: fs,
+      positions: positions,
+      colors: colors,
+      opacities: opacities,
+      sizes: sizes,
+      texture: createTexture(),
+      blending: THREE.NormalBlending
+    });
+    created_points = true;
+  };
+
+  var applyForceToPoints = function() {
+    for (var i = 0; i < movers.length; i++) {
+      var mover = movers[i];
+      var rad1 = Util.getRadian(Util.getRandomInt(0, 360));
+      var rad2 = Util.getRadian(Util.getRandomInt(0, 360));
+      var scalar = Util.getRandomInt(40, 80);
+      mover.is_activate = false;
+      mover.applyForce(Util.getSpherical(rad1, rad2, scalar));
+    }
+  };
+
+  var updateMover =  function() {
+    for (var i = 0; i < movers.length; i++) {
+      var mover = movers[i];
+      mover.time++;
+      if (mover.acceleration.length() < 1) {
+        mover.is_activate = true;
+      }
+      if (mover.is_activate) {
+        mover.applyHook(0, 0.18);
+        mover.applyDrag(0.26);
+      } else {
+        mover.applyDrag(0.035);
+      }
+      mover.updateVelocity();
+      mover.updatePosition();
+      mover.position.sub(points.position);
+      positions[i * 3 + 0] = mover.position.x - points.position.x;
+      positions[i * 3 + 1] = mover.position.y - points.position.x;
+      positions[i * 3 + 2] = mover.position.z - points.position.x;
+      mover.size = Math.log(Util.getRandomInt(1, 128)) / Math.log(128) * Math.sqrt(document.body.clientWidth);
+      sizes[i] = mover.size;
+    }
+    points.updatePoints();
+  };
+
+  var createTexture = function() {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var grad = null;
+    var texture = null;
+
+    canvas.width = 200;
+    canvas.height = 200;
+    grad = ctx.createRadialGradient(100, 100, 20, 100, 100, 100);
+    grad.addColorStop(0.2, 'rgba(255, 255, 255, 1)');
+    grad.addColorStop(0.5, 'rgba(255, 255, 255, 0.3)');
+    grad.addColorStop(1.0, 'rgba(255, 255, 255, 0)');
+    ctx.fillStyle = grad;
+    ctx.arc(100, 100, 100, 0, Math.PI / 180, true);
+    ctx.fill();
+
+    texture = new THREE.Texture(canvas);
+    texture.minFilter = THREE.NearestFilter;
+    texture.needsUpdate = true;
+    return texture;
+  };
+
+  Sketch.prototype = {
+    init: function(scene, camera) {
+      loadImage(function() {
+        getImageData();
+        buildPoints(scene);
+      });
+      camera.range = 1400;
+      camera.rad1_base = Util.getRadian(0);
+      camera.rad1 = camera.rad1_base;
+      camera.rad2 = Util.getRadian(0);
+      camera.setPositionSpherical();
+    },
+    remove: function(scene, camera) {
+      points.geometry.dispose();
+      points.material.dispose();
+      scene.remove(points.obj);
+      image_vertices = [];
+      movers = [];
+      camera.range = 1000;
+    },
+    render: function(scene, camera) {
+      if (created_points) {
+        updateMover();
+        points.updatePoints();
+      }
+      camera.applyHook(0, 0.025);
+      camera.applyDrag(0.2);
+      camera.updateVelocity();
+      camera.updatePosition();
+      camera.lookAtCenter();
+
+    },
+    touchStart: function(scene, camera, vector_mouse_down, vector_mouse_move) {
+      applyForceToPoints();
+    },
+    touchMove: function(scene, camera, vector_mouse_down, vector_mouse_move) {
+      camera.anchor.z = vector_mouse_move.x * 1000;
+      camera.anchor.y = vector_mouse_move.y * -1000;
+    },
+    touchEnd: function(scene, camera, vector_mouse_end) {
+      camera.anchor.z = 0;
+      camera.anchor.y = 0;
+    }
+  };
+
+  return Sketch;
+};
+
+module.exports = exports();
---
+++ src/js/main.js
@@ -0,0 +1,145 @@
+var Util = require('./modules/util');
+var debounce = require('./modules/debounce');
+var Camera = require('./modules/camera');
+var ImageData = require('./image_data');
+
+var body_width = document.body.clientWidth;
+var body_height = document.body.clientHeight;
+var vector_mouse_down = new THREE.Vector2();
+var vector_mouse_move = new THREE.Vector2();
+var vector_mouse_end = new THREE.Vector2();
+
+var canvas = null;
+var renderer = null;
+var scene = null;
+var camera = null;
+var running = new ImageData();
+
+var initThree = function() {
+  canvas = document.getElementById('canvas');
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  if (!renderer) {
+    alert('Three.jsの初期化に失敗しました。');
+  }
+  renderer.setSize(body_width, body_height);
+  canvas.appendChild(renderer.domElement);
+  renderer.setClearColor(0xefefef, 1.0);
+
+  scene = new THREE.Scene();
+
+  camera = new Camera();
+  camera.init(body_width, body_height);
+};
+
+var init = function() {
+  initThree();
+  running.init(scene, camera);
+  renderloop();
+  setEvent();
+  debounce(window, 'resize', function(event){
+    resizeRenderer();
+  });
+};
+
+var getParameterByName = function(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  var results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+var render = function() {
+  renderer.clear();
+  running.render(scene, camera, vector_mouse_move);
+  renderer.render(scene, camera.obj);
+};
+
+var renderloop = function() {
+  var now = Date.now();
+  requestAnimationFrame(renderloop);
+  render();
+};
+
+var resizeRenderer = function() {
+  body_width  = document.body.clientWidth;
+  body_height = document.body.clientHeight;
+  renderer.setSize(body_width, body_height);
+  camera.resize(body_width, body_height);
+};
+
+var setEvent = function () {
+  canvas.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+  });
+
+  canvas.addEventListener('selectstart', function (event) {
+    event.preventDefault();
+  });
+
+  canvas.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+    touchStart(event.clientX, event.clientY, false);
+  });
+
+  canvas.addEventListener('mousemove', function (event) {
+    event.preventDefault();
+    touchMove(event.clientX, event.clientY, false);
+  });
+
+  canvas.addEventListener('mouseup', function (event) {
+    event.preventDefault();
+    touchEnd(event.clientX, event.clientY, false);
+  });
+
+  canvas.addEventListener('touchstart', function (event) {
+    event.preventDefault();
+    touchStart(event.touches[0].clientX, event.touches[0].clientY, true);
+  });
+
+  canvas.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+    touchMove(event.touches[0].clientX, event.touches[0].clientY, true);
+  });
+
+  canvas.addEventListener('touchend', function (event) {
+    event.preventDefault();
+    touchEnd(event.changedTouches[0].clientX, event.changedTouches[0].clientY, true);
+  });
+
+  window.addEventListener('mouseout', function () {
+    event.preventDefault();
+    touchEnd(0, 0, false);
+  });
+};
+
+var transformVector2d = function(vector) {
+  vector.x = (vector.x / body_width) * 2 - 1;
+  vector.y = - (vector.y / body_height) * 2 + 1;
+};
+
+var touchStart = function(x, y, touch_event) {
+  vector_mouse_down.set(x, y);
+  transformVector2d(vector_mouse_down);
+  if (running.touchStart) running.touchStart(scene, camera, vector_mouse_down);
+};
+
+var touchMove = function(x, y, touch_event) {
+  vector_mouse_move.set(x, y);
+  transformVector2d(vector_mouse_move);
+  if (running.touchMove) running.touchMove(scene, camera, vector_mouse_down, vector_mouse_move);
+};
+
+var touchEnd = function(x, y, touch_event) {
+  vector_mouse_end.set(x, y);
+  if (running.touchEnd) running.touchEnd(scene, camera, vector_mouse_end);
+};
+
+var switchMenu = function() {
+  btn_toggle_menu.classList.toggle('is-active');
+  menu.classList.toggle('is-active');
+  document.body.classList.remove('is-pointed');
+};
+
+init();
